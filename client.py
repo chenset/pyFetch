a55b2d2251cfe0547c11befac83776029eeff8c6
chenset <4199191@qq.com>
2015-04-11 22:16:04 +0800

    test socket with gevent

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ def client(content):
     # sock.connect(('10.0.0.10', 7777))
     sock.connect(('127.0.0.1', 7777))
 
-    send_date = 's' + (str(content) * 5000) + 'e'
+    send_date = 's' + (str(content) * 1000) + 'e'
 
     # content前10个字符串用于标识内容长度.
     response_len = (str(len(send_date) + 10) + ' ' * 10)[0:10]
@@ -18,7 +18,6 @@ def client(content):
     print 1
     buff_size = 1024
     data = sock.recv(buff_size)
-
 
     # content前10个字符串用于标识内容长度.
     data_len = int(data[0:10])
@@ -34,8 +33,8 @@ def client(content):
 
 start_time = time.time()
 pool = []
-for i in xrange(1):
-    pool.append(gevent.spawn(client, str(i)+'你好'))
+for i in xrange(100):
+    pool.append(gevent.spawn(client, str(i) + '你好'))
 
 gevent.joinall(pool)
 print round((time.time() - start_time) * 1000, 2), 'ms'
