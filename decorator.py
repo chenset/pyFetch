67eb81a2bd488a5286a2fc0558b05adb4aac4061
chenset <4199191@qq.com>
2015-04-19 22:15:06 +0800

    zlib test

--- decorator.py
+++ decorator.py
@@ -18,4 +18,14 @@ def save():
     print 'save'
 
 
-app.run()
+# app.run()
+
+import gzip, zlib
+
+s = [str(x) for x in xrange(10000)]
+ss = '0' * 1000000
+print len(ss)
+print len(zlib.compress(ss, 5))
+# print zlib.compress(ss, 1)
+print zlib.decompress(zlib.compress(ss, 1))
+
